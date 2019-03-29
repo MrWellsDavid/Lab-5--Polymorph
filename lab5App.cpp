4883782c1b03c4b49e3ebfc098260330fb3ce296
jack-valin <48726722+jack-valin@users.noreply.github.com>
2019-03-29 19:16:01 -0400

    Functioning viewAll
    Resolved a logical error (double iteration). This is now functional.

--- lab5App.cpp
+++ lab5App.cpp
@@ -37,9 +37,6 @@ void addFaculty(Faculty* fPTR, int size, int &track, Employee** ePTR, int empSiz
 		for (int i = 0; i < track; i++){
 			fPTR++;
 		}
-		for (int i = 0; i < empTrack; i++){
-			ePTR++;
-		}
 		cout <<"Enter full name: ";
 		cin >>first>>last;
 		cout <<"Enter the id: ";
@@ -102,9 +99,6 @@ void addStudent(Student* sPTR, int size, int &track, Employee** ePTR, int empSiz
 		for (int i = 0; i < track; i++){
 			sPTR++;
 		}
-		for (int i = 0; i < empTrack; i++){
-			ePTR++;
-		}
 		cout <<"Enter full name: ";
 		cin >>first>>last;
 		cout <<"Enter the id: ";
@@ -165,16 +159,11 @@ void viewStudent(Student* sPTR, int max)
 		sPTR++;
 	}
 }
-// Jack's been having some trouble with this.
-// Here's some references Jack's been playing around with:
-// https://stackoverflow.com/questions/9144401/c-dereferencing-pointers-that-are-elements-of-an-array
-// The syntax described here (https://stackoverflow.com/questions/28641955/void-value-not-ignored-as-it-ought-to-be)...
-// works if there's just one item in the Employee array, but if there's more than one, it seems to crash the program.
 void viewAll(Employee** ePTR, int empMax){
 	for (int i = 0; i < empMax; i++)
 	{
 		(*ePTR) -> print();
-		ePTR++; // Since it crashes if there's more than one, I suspect we can't just increment the pointers like this
+		ePTR++;
 	}
 }
 int main(int argc, char** argv) {
